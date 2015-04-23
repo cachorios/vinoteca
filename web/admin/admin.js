63c80cc5c5a232bed839ed413544f42eaabb37a4
albertoe2003 <albertovoeffray@gmail.com>
2015-04-23 09:54:09 -0300

    sql bases y js textarea

--- web/admin/admin.js
+++ web/admin/admin.js
@@ -1,14 +1,15 @@
 // keep track of how many email fields have been rendered
 jQuery(document).ready(function () {
+    jQuery('textarea').autosize();
+});
 
-})
-
-(function($) {
-    //$(document).ready(function(){
-    //    var testModal = $(".modal");
-    //    if(testModal.length > 0) {
-    //        testModal.modal("show");
-    //    }
-    //});
-})(jQuery);
+/* producto metadatos lista*/
+jQuery().ready(function () {
+    jQuery('.select-customopengraph .control select').change(function () {
+        if (1 == this.options[this.selectedIndex].value) {
+            jQuery('.select-customopengraph .og-textarea').show()
+        } else jQuery('.select-customopengraph .og-textarea').hide()
+    });
+    jQuery('.select-customopengraph .control select').change()
+});
 
