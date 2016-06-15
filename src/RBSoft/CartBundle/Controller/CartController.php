1184cdd20cfa22f5d7b874348296321c896c98dc
cachorios <cachorios@gmail.com>
2016-06-15 09:25:30 -0300

    cart

--- src/RBSoft/CartBundle/Controller/CartController.php
+++ src/RBSoft/CartBundle/Controller/CartController.php
@@ -36,7 +36,7 @@ class CartController extends Controller
      * @return \Symfony\Bundle\FrameworkBundle\Controller\Response
      * @Route("/cartdisplay/{size}", name="cartdisplay")
      */
-    public function displayCartAction($size = "big", $toHtml = false)
+    public function displayCartAction($size = "big")
     {
         $size = strtolower($size);
         if (!in_array($size, array('big', 'medium', 'small'))) {
@@ -54,10 +54,9 @@ class CartController extends Controller
         $cartManager = $this->get('rbsoft.cartManager');
         $cart = $cartManager->getCart();
 
-
-        $this->get('ladybug')->log("tres",$cart);
         //preciso recargar los productos para que se vean las iamagenes
         $cartManager->refreshProductos();
+
 //        $logger = $this->get('logger');
 //        $logger->debug("display cart : cart=".print_r($cart,true));
 
@@ -70,7 +69,7 @@ class CartController extends Controller
         );
 
 
-        if(!$toHtml) {
+        //if( $size == 'Big' ) {
            return $this->render(
                 'RBSoftCartBundle:Cart:display' . $size . 'Cart.html.twig',
                 array(
@@ -79,16 +78,15 @@ class CartController extends Controller
                     'cartManagerModel' => $cartManager
                 )
             );
-        }else{
-
-           return $this->renderView('RBSoftCartBundle:Cart:display' . $size . 'Cart.html.twig',
-               array(
-                   'cart' => $displayCart,
-                   'cartModel' => $cart,
-                   'cartManagerModel' => $cartManager
-               )
-           );
-        }
+//        }else{
+//           return $this->renderView('RBSoftCartBundle:Cart:display' . $size . 'Cart.html.twig',
+//               array(
+//                   'cart' => $displayCart,
+//                   'cartModel' => $cart,
+//                   'cartManagerModel' => $cartManager
+//               )
+//           );
+//        }
     }
 
     /**
@@ -109,8 +107,19 @@ class CartController extends Controller
         $cart = $cartManager->getCart();
         $cart->deleteItem($lineId);
 
+        $displayCart = array(
+            "total" => $cartManager->getCarroTotal() ,
+            "cantidad" => $cartManager->getCantidad(),
+            'items' => array(
+            )
+        );
         $contenido = array(
-            "#cart"     => $this->displayCartAction('small',true),
+            '#cart' => $this->renderView('RBSoftCartBundle:Cart:displaySmallCart.html.twig',
+                array(
+                    'cart' => $displayCart,
+                    'cartModel' => $cart,
+                    'cartManagerModel' => $cartManager
+                )),
             "#subtotal" => sprintf(' $ %10.2f', $cartManager->getCarroTotal()),
             //"#total" => $this->displayCartAction('big',true),
         );
@@ -123,7 +132,7 @@ class CartController extends Controller
 
     /**
      * @Route(
-     *  "/cartupdateitem/{lineId}/{cantidad}/{modo}",
+     *  "/cartupdateitem/{lineId}/{cantidad}",
      *  name="cartupdateitem",
      *  options={"expose"=true} )
      *
@@ -131,23 +140,46 @@ class CartController extends Controller
      * @param int $cantidad
      * @return mixed
      */
-    public function UpdateLineAction($lineId, $cantidad = 0, $modo='small')
+    public function UpdateLineAction($lineId, $cantidad = 0)
     {
-        //$targetUrl = $request->query->get('rbsoft_cart_target_url');
+
         $cartManager = $this->get('rbsoft.cartManager');
         $cart = $cartManager->getCart();
         $cart->UdateItemCantidad($lineId, $cantidad);
 
-        $this->get('ladybug')->log("Unooooo",$cart);
+        //preciso recargar los productos para que se vean las iamagenes
+        $cartManager->refreshProductos();
+
+        // build view object
+        $displayCart = array(
+            "total" => $cartManager->getCarroTotal() ,
+            "cantidad" => $cartManager->getCantidad(),
+            'items' => array(
+            )
+        );
+
         $contenido = array(
-                $modo == 'samll' ? "#cart" : '#cartBig'     => $this->displayCartAction($modo,true),
+                '#shopping-cart-table'     => $this->renderView('RBSoftCartBundle:Cart:cartdisplay.html.twig',
+                    array(
+                        'cart' => $displayCart,
+                        'cartModel' => $cart,
+                        'cartManagerModel' => $cartManager
+                    )),
+                '#cart' => $this->renderView('RBSoftCartBundle:Cart:displaySmallCart.html.twig',
+                    array(
+                        'cart' => $displayCart,
+                        'cartModel' => $cart,
+                        'cartManagerModel' => $cartManager
+                    )),
                 "#subtotal" => sprintf(' $ %10.2f', $cartManager->getCarroTotal()),
-                //"#total" => $this->displayCartAction('big',true),
+
             );
 
         $response = new Response(json_encode($contenido));
         return $response;
     }
+    
+    
 
 
 }
